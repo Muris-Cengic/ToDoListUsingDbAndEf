b47ba2bf41df0ecddbe7195b7afa0e9fdeb7bf21
Muris.Cengic <muris.cengic@outlook.com>
2024-03-08 11:36:05 +0400

    Add initial example of how to add and retrieve data from db

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ToDoListUsingDbAndEf
 {
@@ -10,6 +7,81 @@ namespace ToDoListUsingDbAndEf
     {
         static void Main(string[] args)
         {
+            initializeDatabaseData();
+
+            using (var db = new todolistEntities())
+            {
+                foreach (var category in db.Categories)
+                {
+                    Console.WriteLine("========================================================");
+                    Console.WriteLine($"Category: {category.CategoryName}");
+                    Console.WriteLine("Tasks:");
+
+                    foreach (var task in category.Tasks)
+                    {
+                        Console.WriteLine($"      {task.Title} - {task.DateCreated}");
+                    }
+                }    
+            }
+            Console.ReadKey();
+        }
+
+        private static void initializeDatabaseData()
+        {
+            // Category Midterm
+            Category c1 = new Category()
+            {
+                CategoryID = 1,
+                CategoryName = "Midterm",
+            };
+            Task t1 = new Task()
+            {
+                TaskID = 1,
+                Category = c1,
+                Title = "Prepare notes for OOP",
+                DateCreated = DateTime.Now,
+            };
+
+            // Category E Skills
+            Category c2 = new Category()
+            {
+                CategoryID = 2,
+                CategoryName = "E Skills 24"
+            };
+
+            Task t2 = new Task()
+            {
+                TaskID = 2,
+                Category = c2,
+                Title = "Wath a video about Entity Framework",
+                DateCreated = DateTime.Now,
+            };
+            Task t3 = new Task()
+            {
+                TaskID = 3,
+                Category = c2,
+                Title = "Wath a video about LINQ",
+                DateCreated = DateTime.Now,
+            };
+
+            // Add records to database if database is empty
+            using (var db = new todolistEntities())
+            {
+                if (!db.Categories.Any())
+                {
+                    db.Categories.AddObject(c1);
+                    db.Categories.AddObject(c2);
+                }
+
+                if (!db.Tasks.Any())
+                {
+                    db.Tasks.AddObject(t1);
+                    db.Tasks.AddObject(t2);
+                    db.Tasks.AddObject(t3);
+                }
+
+                db.SaveChanges();
+            }
         }
     }
 }
